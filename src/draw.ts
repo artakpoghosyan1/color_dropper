523d4d7226cfa5a9c5bf049495404044e9442f63
artak <artak@brandlens.io>
2024-05-24 16:57:04 +0400

    draw image and integrate webpack

---
+++ src/draw.ts
@@ -0,0 +1,45 @@
+import image from './image.jpg';
+
+export function draw(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+    const img = new Image();
+
+    function getImageSize(): {width: number, height: number} {
+        const imgRatio = img.width / img.height;
+        const canvasRatio = canvas.width / canvas.height;
+        let drawWidth, drawHeight;
+
+        if (canvasRatio < imgRatio) {
+            drawWidth = canvas.width;
+            drawHeight = drawWidth / imgRatio;
+        } else {
+            drawHeight = canvas.height;
+            drawWidth = drawHeight * imgRatio;
+        }
+
+        return {
+            width: drawWidth,
+            height: drawHeight
+        }
+    }
+
+    function drawImage() {
+        const {width, height} = getImageSize()
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, width, height);
+    }
+
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        drawImage();
+    }
+
+    img.onload = function() {
+        resizeCanvas()
+    };
+
+    img.src = image;
+
+    window.addEventListener('resize', resizeCanvas);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-const x: number = 12
-console.log(x)
+import {draw} from "./draw";
+
+window.onload = function() {
+    const canvas: HTMLElement | null = document.getElementById('canvas');
+    if(canvas) {
+        const ctx: CanvasRenderingContext2D | null = (canvas as HTMLCanvasElement).getContext("2d");
+        draw(canvas as HTMLCanvasElement, ctx!)
+    }
+}
